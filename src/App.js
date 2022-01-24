0bc1fb8948b713e42a98e4e9652f2604b273a357
Dania <daniadeyar@gmail.com>
2022-01-24 20:04:21 +0300

    Co-authored-by: Ahmad Aras <Ahmad0811@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
-
 const url = "https://course-api.com/react-tabs-project";
 function App() {
   return <h2>tabs project setup</h2>;
